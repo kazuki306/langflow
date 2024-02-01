86bfa00c81c6058d420ebff33619ba9f664cadef
nsxshota <nsxshota@amazon.co.jp>
2024-02-01 14:33:38 +0900

    api gw + NLB arch

--- scripts/aws/lib/construct/network.ts
+++ scripts/aws/lib/construct/network.ts
@@ -4,31 +4,33 @@ import {
   aws_ec2 as ec2,
   aws_ecs as ecs,
   aws_logs as logs,
-  aws_servicediscovery as servicediscovery,
+  aws_apigateway as apigateway,
   aws_elasticloadbalancingv2 as elb,
 } from 'aws-cdk-lib';
+import { SubnetType } from 'aws-cdk-lib/aws-ec2';
 
 export class Network extends Construct {
   readonly vpc: ec2.Vpc;
   readonly cluster: ecs.Cluster;
-  readonly cloudmapNamespace: servicediscovery.PrivateDnsNamespace;
   readonly ecsBackSG: ec2.SecurityGroup;
+  readonly backendServicePort: number
   readonly dbSG: ec2.SecurityGroup;
   readonly backendLogGroup: logs.LogGroup;
-  readonly alb: elb.IApplicationLoadBalancer;
-  readonly albTG: elb.ApplicationTargetGroup;
-  readonly albSG: ec2.SecurityGroup;
+  readonly nlb: elb.INetworkLoadBalancer;
+  readonly nlbTG: elb.NetworkTargetGroup;
+  readonly nlbSG: ec2.SecurityGroup;
+  readonly api: apigateway.RestApi;
 
   constructor(scope: Construct, id: string) {
     super(scope, id)
-    const alb_listen_port=80
-    const back_service_port=7860
+    const nlb_listen_port=80
+    this.backendServicePort = 7860
 
     // VPC等リソースの作成
     this.vpc = new ec2.Vpc(scope, 'VPC', {
       vpcName: 'langflow-vpc',
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
-      maxAzs: 3,
+      maxAzs: 2,
       subnetConfiguration: [
         {
           cidrMask: 24,
@@ -49,35 +51,67 @@ export class Network extends Construct {
       natGateways: 1,
     })
 
-    // ALBに設定するセキュリティグループ
-    this.albSG = new ec2.SecurityGroup(scope, 'ALBSecurityGroup', {
-      securityGroupName: 'alb-sg',
-      description: 'for alb',
+    // NLBの設定
+    this.nlbSG = new ec2.SecurityGroup(scope, 'ALBSecurityGroup', {
+      securityGroupName: 'nlb-sg',
+      description: 'for nlb',
       vpc: this.vpc,
     })
 
-    this.alb = new elb.ApplicationLoadBalancer(this,'langflow-alb',{
-      internetFacing: true, //インターネットからのアクセスを許可するかどうか指定
-      loadBalancerName: 'langflow-alb',
-      securityGroup: this.albSG, //作成したセキュリティグループを割り当てる
-      vpc:this.vpc,   
+    this.nlb = new elb.NetworkLoadBalancer(this,'langflow-nlb',{
+      vpc:this.vpc,
+      vpcSubnets:{subnetType: SubnetType.PRIVATE_WITH_EGRESS},
+      loadBalancerName: 'langflow-nlb',
+      crossZoneEnabled:true,
+      internetFacing:false,
+      securityGroups:[this.nlbSG]
     })
 
-    const listener = this.alb.addListener('Listener', { port: alb_listen_port });
+    this.nlbTG = new elb.NetworkTargetGroup(this, `TargetGroup`, {
+      targetGroupName: 'ecs-tagtet-group',
+      vpc:this.vpc,
+      port: nlb_listen_port,
+      targetType: elb.TargetType.IP,
+          deregistrationDelay: Duration.seconds(0) // 開発環境デプロイ高速化のため。本番環境は数値上げておく
+      });
+      
+      this.nlb.addListener(`Listener`, {
+          port: this.backendServicePort,
+          defaultTargetGroups: [this.nlbTG]
+      });
+    //
 
-    this.albTG = listener.addTargets('targetGroup', {
-      port: back_service_port,
-      protocol: elb.ApplicationProtocol.HTTP,
-      healthCheck: {
-        enabled: true,
-        path: '/health',
-        healthyThresholdCount: 2,
-        unhealthyThresholdCount: 4,
-        interval: Duration.seconds(100),
-        timeout: Duration.seconds(30),
-        healthyHttpCodes: '200',
-      },
+    // VPC link
+    const vpcLink = new apigateway.VpcLink(this, `NLB-VpcLink`, {
+      vpcLinkName: 'api-vpc-link',
+      description: 'Connect to the NLB API.',
+      targets: [this.nlb]
     });
+    
+    // APIGW 
+    this.api = new apigateway.RestApi(this, 'Api',{
+      deployOptions: {
+        stageName: 'api',
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+      cloudWatchRole: true,
+    });
+
+    this.api.root.addMethod('ANY', new apigateway.Integration({
+      type: apigateway.IntegrationType.HTTP_PROXY,
+      integrationHttpMethod: 'ANY',
+      uri: `http://${this.nlb.loadBalancerDnsName}`,
+      options: {
+          connectionType: apigateway.ConnectionType.VPC_LINK,
+          vpcLink: vpcLink
+    }}), {}
+    );
+
+    // vpc-link(vpc cidr) から nlbへのinbound 許可
+    this.nlbSG.addIngressRule(ec2.Peer.ipv4(this.vpc.vpcCidrBlock), ec2.Port.tcp(nlb_listen_port))
 
     // Cluster
     this.cluster = new ecs.Cluster(this, 'EcsCluster', {
@@ -86,23 +120,13 @@ export class Network extends Construct {
       enableFargateCapacityProviders: true,
     });
 
-    // Private DNS
-    this.cloudmapNamespace = new servicediscovery.PrivateDnsNamespace(
-      this,
-      'Namespace',
-      {
-        name: 'ecs-deploy.com',
-        vpc: this.vpc,
-      }
-    );
-
     // ECS BackEndに設定するセキュリティグループ
     this.ecsBackSG = new ec2.SecurityGroup(scope, 'ECSBackEndSecurityGroup', {
       securityGroupName: 'langflow-ecs-back-sg',
       description: 'for langflow-back-ecs',
       vpc: this.vpc,
     })
-    this.ecsBackSG.addIngressRule(this.albSG,ec2.Port.tcp(back_service_port))
+    this.ecsBackSG.addIngressRule(this.nlbSG,ec2.Port.tcp(this.backendServicePort))
 
     // RDSに設定するセキュリティグループ
     this.dbSG = new ec2.SecurityGroup(scope, 'DBSecurityGroup', {
